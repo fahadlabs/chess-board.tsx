6ef37525d894186368f37de07dccff5793b4214b
Fahad Hossain <fahadh.dev@gmail.com>
2022-03-06 22:44:11 +0600

    Chess board added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const board: boolean[][] = Array(8)
+  .fill(0)
+  .map((_, i) =>
+    Array(8)
+      .fill(0)
+      .map((_, j) => i % 2 === j % 2)
+  );
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        {board.map((row, i) => (
+          <div key={i} className='row'>
+            {row.map((val, j) => (
+              <div key={j} className={`item ${val ? 'white' : 'black'}`} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
